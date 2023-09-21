260adbf39ca719f957cb69f446eb62ea69858340
nasre21 <nasou2001@gmail.com>
2023-09-21 14:31:07 +0200

    its gone router child

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -5,12 +5,18 @@ import { ContactsComponent } from './contacts/contacts.component';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { UserComponent } from './users/user/user.component';
+import { ListsComponent } from './users/lists/lists.component';
+import { DetailsComponent } from './users/details/details.component';
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'contact-reactive', component: ContactReactiveComponent },
   { path: 'contact-template/:id', component: ContactsComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'users', component: UserComponent },
+  { path: 'user', component: UserComponent , children: [
+    {path:'lists', component: ListsComponent},
+    {path:'details', component: DetailsComponent}
+
+  ] },
   { path:'**', component: PageNotFoundComponent}
 ];
 
